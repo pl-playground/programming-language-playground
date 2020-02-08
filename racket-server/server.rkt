35f9fbc27f8d0ab6c389dd8c41286669ae5c8902
Atharva Shukla <shukla.at@husky.neu.edu>
2020-02-08 15:39:07 -0500

    add PS, and make interp folder, refactor

--- racket-server/server.rkt
+++ racket-server/server.rkt
@@ -2,22 +2,35 @@
 
 (require web-server/servlet
          web-server/servlet-env
-         "interp1.rkt")
+         "interpreters/interp1.rkt")
 
+; Repl is a
+; (list Source : String
+;       Value  : String)
 
- 
+; Repl when no binding exists
+(define NO-BINDING-REPL (list "" ""))
+
+; Request -> Repl
+; returns a REPL with src code from textarea 
+; and the the interpreted value. If no binding,
+; returns NO-BINDING-REPL
 (define (binding-handler request)
   (define bindings (request-bindings request))
   (if (exists-binding? 'name bindings)
       (let ([b (extract-binding/single 'name bindings)])
         (list b (interp1 b)))
-      (list "" "")))
+      NO-BINDING-REPL))
 
+; Request -> Xexpr
+; wraps the src code from Repl into a text area
 (define (display-code request)
   `(textarea ((name "name") (id "myTextArea") (rows "20") (cols "80"))
              ,(first (binding-handler request))))
 
-(define (myresponse request)
+; Request -> Response
+; response to server with Xexpr
+(define (main request)
   (define bindings (request-bindings request))
   (response/xexpr
    `(html (head (title "PL Playground"))
@@ -28,4 +41,5 @@
             (p (input ((type "Submit"))))
             (p ,(second (binding-handler request))))))))
 
-(serve/servlet myresponse #:port 8080)
+; start the server
+(serve/servlet main #:port 8080)
