29c2fb1f5c47613003f014ea6509b9f1df94d971
Atharva Shukla <shukla.at@husky.neu.edu>
2020-02-08 15:27:39 -0500

    doesn't modify src code

--- racket-server/testing.rkt
+++ racket-server/testing.rkt
@@ -4,15 +4,18 @@
          web-server/servlet-env
          "interp1.rkt")
 
+
+ 
 (define (binding-handler request)
   (define bindings (request-bindings request))
   (if (exists-binding? 'name bindings)
-      (interp1 (extract-binding/single 'name bindings))
-      ""))
+      (let ([b (extract-binding/single 'name bindings)])
+        (list b (interp1 b)))
+      (list "" "")))
 
 (define (display-code request)
-  `(textarea ((name "name") (id "myTextArea")(rows "20") (cols "80"))
-             ,(binding-handler request)))
+  `(textarea ((name "name") (id "myTextArea") (rows "20") (cols "80"))
+             ,(first (binding-handler request))))
 
 (define (myresponse request)
   (define bindings (request-bindings request))
@@ -23,6 +26,6 @@
            (form
             ,(display-code request)
             (p (input ((type "Submit"))))
-            (p ,(binding-handler request)))))))
+            (p ,(second (binding-handler request))))))))
 
 (serve/servlet myresponse #:port 8080)
