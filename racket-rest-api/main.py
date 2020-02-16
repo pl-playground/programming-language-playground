61bcb7db702ef2f9b62aac23c1e28c86ffbc9fcf
nsaripalli <nithinsaripalli@gmail.com>
2020-02-15 20:07:34 -0500

    adding cleaned input for prevent bad people. Also adding server name

--- racket-rest-api/main.py
+++ racket-rest-api/main.py
@@ -8,36 +8,36 @@ from starlette.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:4200",
-    "http://localhost:*",
-    "null",
-    # This is for the cloud?
-    "http://example.appspot.com"
+  "http://localhost",
+  "http://localhost:8080",
+  "http://localhost:4200",
+  "http://localhost:*",
+  "http://programming-language-playground.com/"
 ]
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
 )
 
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "Matthias. Tell Amal I said Hi."}
+  return {"Hello": "Matthias."}
+
 
 class Item(BaseModel):
-    interpret_name: str
-    user_input: str
+  interpret_name: str
+  user_input: str
 
 
 @app.post("/code/")
-async def read_item(item:  Item):
-    cmd = ["racket", f"{item.interpret_name}.rkt", item.user_input]
-    racket_output = subprocess.check_output(cmd).decode()
+async def read_item(item: Item):
+  clean_interpret_name = ''.join(e for e in item.interpret_name if e.isalnum())
+  cmd = ["racket", f"{clean_interpret_name}.rkt", item.user_input]
+  racket_output = subprocess.check_output(cmd).decode()
 
-    return {"output": racket_output}
+  return {"output": racket_output}
